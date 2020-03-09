bdf35f956076677fcde7785f4352571cfd9e0a0e
ernestkoko <ernesteferetin@yahoo.com>
2020-03-09 11:32:19 +0100

    Added the Data Access Object, DAO. And wrote the necessary queries

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,27 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao // this tells the DB to use this as the Data Access Object(DAO)
+interface SleepDatabaseDao{
+
+    @Insert  //this tells Room to insert into the DB
+    fun insert(night: SleepNight) // the funtion inserts SleepNight table
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query(value = "SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight
+
+    @Query(value = "DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query(value = "SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query(value = "SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
