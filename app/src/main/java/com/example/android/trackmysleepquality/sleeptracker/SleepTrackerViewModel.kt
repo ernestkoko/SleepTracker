96d04824789eaca6e466bb3a674056bdad58be90
ernestkoko <ernesteferetin@yahoo.com>
2020-03-09 16:33:11 +0100

    Added coroutine to make the app run on other thread apart from main

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,7 +18,12 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -26,5 +31,84 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+    //this Job allows us cancel all coroutine started by this job when not needed
+    private var viewModelJob = Job()
+
+    //called when the ViewModel is not needed
+    override fun onCleared() {
+        super.onCleared()
+        //this cancels whe Job when the ViewModel dies
+        viewModelJob.cancel()
+    }
+
+    //the scope dtermines what thread the coroutine will run on
+    private val uisScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var tonight = MutableLiveData<SleepNight?>()
+    private val nights = database.getAllNights()
+
+    //formating the to strings. it executes every time night changes
+    val nightsString = Transformations.map(nights){nights ->
+        formatNights(nights, application.resources)
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        uisScope.launch {
+            tonight.value = getTonightFromDatabae()
+        }
+    }
+
+    private suspend fun getTonightFromDatabae(): SleepNight? { // the suspend keyword ensures the function does not bock
+        return withContext(Dispatchers.IO){
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli){
+                night = null
+            }
+            night
+        }
+
+    }
+    fun onStartTracking(){
+        uisScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabae()
+        }
+    }
+    private suspend fun insert(night: SleepNight){
+        withContext(Dispatchers.IO){
+            database.insert(night)
+        }
+    }
+    fun onStopTracking(){
+        uisScope.launch{
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+    private suspend fun update(night: SleepNight){
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+
+    }
+    fun onClear(){
+        uisScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+    suspend fun clear(){
+        withContext(Dispatchers.IO){
+            database.clear()
+        }
+    }
+
 }
 
